f9bad22fcc2de494df60af326a5cbc876515bab9
kimseongmin0301 <pose1358@naver.com>
2023-09-18 08:15:08 +0900

    first commit

---
+++ src/main/java/com/organized/comilation/ComilationApplication.java
@@ -0,0 +1,13 @@
+package com.organized.comilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComilationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComilationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organized/comilation/ComilationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organized.comilation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComilationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
